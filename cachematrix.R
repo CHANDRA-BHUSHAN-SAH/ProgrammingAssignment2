9c3599f5b1785594fee99b76cb168cf11e787567
CHANDRA-BHUSHAN-SAH <chandrabhushan.sah@live.in>
2015-01-23 12:51:26 +0530

    Assignment2: Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,27 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## Matrix variable invrs to store inverted matrix
+  invrs <- NULL
+
+## set function to set the original matrix i.e. x with some value or another matrix
+  set <- function(y) {
+    x <<- y
+    invrs <<- NULL
+  }
+
+## get function to retrieve the value of the original matrix i.e. x
+  get <- function() x
+
+## setInvers function to cache the inverse of matrix x
+  setInvers <- function(inv) invrs <<- inv
+
+##  getInvers function to retrieve  the cached value of inverse matrix i.e. invrt
+  getInvers <- function() invrs
+
+  list(set = set, get = get,
+       setInvers = setInvers,
+       getInvers = getInvers)
 }
 
 
@@ -12,4 +33,24 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+## checking whether the inverse is calculated or not
+  invrs <- x$getInvers()
+  if(!is.null(invrs)) {
+    
+    ## the inverse is already calculated to returning the cached value
+    message("getting cached data")
+    return(invrs)
+  }
+
+## inverse of matrix is not calculated so inversing the matrix and caching it
+## getting data of original matrix in data variable through get() function
+  data <- x$get()
+
+  ## inversing the data through solve() function
+  invrs <- solve(data, ...)
+
+  ## caching the inversed matrix through setInvers() function
+  x$setInvers(invrs)
+  invrs
 }
